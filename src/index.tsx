35c766f35eee80a335e879be76b798196a26723c
haeunbae <baehaeun96@gmail.com>
2022-01-21 03:43:01 +0900

    sentry 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import App from './App';
-
-Sentry.init({
-    dsn: 'https://a78c3b37c74f4479baa508ddbeca8bae@o1106768.ingest.sentry.io/6133487',
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0
-});
 
 (() => {
     if (window.Kakao?.Auth) return;
