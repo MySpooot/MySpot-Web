fd89edce80c7c316fea21dbac09061b2517aeeba
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-01-29 15:16:15 +0900

    lint

--- src/pages/MyMap/MyMap.tsx
+++ src/pages/MyMap/MyMap.tsx
@@ -2,14 +2,12 @@ import React, { FC } from 'react';
 import { Outlet, useParams } from 'react-router';
 import { useQuery } from 'react-query';
 
-import { Path } from 'src/Constants';
 import { getMapDetail } from 'src/api/map';
-import Loading from 'src/components/Loading';
 
 const MyMap: FC = () => {
     const params = useParams<{ mapId: string }>();
 
-    const { data: mapDetail } = useQuery(['getMapDetail', params.mapId], () => getMapDetail({ mapId: Number(params.mapId) }));
+    useQuery(['getMapDetail', params.mapId], () => getMapDetail({ mapId: Number(params.mapId) }));
 
     return <Outlet />;
 };
