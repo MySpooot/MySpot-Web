c6271bf9563d7ddd6f3cd9d88055f6537c12708c
haeunbae <baehaeun96@gmail.com>
2022-03-02 20:24:35 +0900

    my page update

--- src/components/NewMapModal/styles.ts
+++ src/components/NewMapModal/styles.ts
@@ -1,32 +1,28 @@
 import styled from '@emotion/styled';
 
-import { BreakPoint, Palette, Dimension } from 'src/Constants';
+import { Palette, Dimension } from 'src/Constants';
 
 export const Container = styled.div<{ open: boolean }>`
     position: absolute;
     z-index: 99;
     bottom: 0;
     display: flex;
-    width: ${Dimension.MaxWidth};
+    width: calc(${Dimension.MaxWidth} - 2.125rem);
     height: ${({ open }) => (open ? '100vh' : '0')};
     flex-direction: column;
-    align-items: center;
     background-color: #ffffff;
     overflow-y: hidden;
     transition: height 0.5s;
-`;
-
-export const Myspot = styled.div`
-    width: 100%;
-    padding-top: 1.125rem;
-    font-size: 1.5rem;
-    font-weight: bold;
+    padding: ${({ open }) => (open ? '1.0625rem' : 0)};
 `;
 
 export const Title = styled.div`
-    margin-top: 50%;
-    font-size: 1.5rem;
+    font-size: 1.625rem;
     font-weight: bold;
+    line-height: 1;
+    letter-spacing: 0.01125rem;
+    color: #333;
+    margin-top: 7.625rem;
 `;
 
 export const Desc = styled.div`
@@ -34,50 +30,58 @@ export const Desc = styled.div`
     font-size: 1rem;
     font-weight: 300;
     line-height: 2;
+    letter-spacing: 0.006875rem;
+    margin-top: 0.3125rem;
 `;
 
 export const InputArea = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
-    margin-top: 15%;
+    margin-top: 1.875rem;
 `;
 
 export const Input = styled.input`
-    width: 19.1875rem;
-    height: 3rem;
-    padding: 0.5rem 1.5rem;
+    height: 1.25rem;
     border: 1px solid ${Palette.Grey[400]};
     border-radius: 0.25rem;
+    margin-bottom: 1.25rem;
+    padding: 0.8125rem;
+
+    &::placeholder {
+        font-size: 0.9375rem;
+        letter-spacing: -0.21px;
+        color: #bebebe;
+    }
 `;
 
 export const ButtonArea = styled.div`
     display: flex;
-    width: 19.625rem;
-    justify-content: space-between;
-    margin-top: 15%;
-
-    ${BreakPoint.PC} {
-        width: 70%;
-    }
-`;
+    margin-top: auto;
+    justify-content:space-between;
+}
+    `;
 
 export const Button = styled.button`
     width: 48%;
-    padding: 1rem 2.5rem;
+    height: 48px;
     border: none;
     border-radius: 0.25rem;
     cursor: pointer;
-    font-weight: bold;
     outline: none;
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 2;
+    letter-spacing: 0.11px;
+    text-align: center;
 
     &:hover {
         opacity: 0.8;
     }
 
-    &:last-child {
-        background-color: ${Palette.Grey[500]};
-        color: ${Palette.Black};
+    &:first-child {
+        color: ${Palette.Grey[500]};
+        border: solid 1px #d3d3d3;
+        background-color: ${Palette.White};
     }
 
     &:last-child {
--- src/pages/MyPage/styles.ts
+++ src/pages/MyPage/styles.ts
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
-import { HiUser } from 'react-icons/hi';
+import { Palette } from 'src/Constants';
 
 export const Container = styled.div`
     display: flex;
     height: 100vh;
+    width: 28.125rem;
     flex-direction: column;
     padding: 0 1.5rem;
 `;
@@ -13,69 +14,91 @@ export const Header = styled.div`
 `;
 export const UserInfo = styled.div`
     display: flex;
-    height: 35%;
-    flex-direction: column;
+    min-height: 6.25rem;
     align-items: center;
     justify-content: space-between;
     padding: 0 1.5rem;
 `;
 
-export const UserNickname = styled.div`
+export const User = styled.div`
     display: flex;
-    width: 100%;
-    height: 20%;
-    justify-content: space-between;
-    font: bold;
-    font-size: 1.5rem;
-
-    .nickname-update {
-        display: flex;
-        width: 60%;
-        justify-content: flex-start;
+    align-items: center;
+    .mypage-img {
+        width: 3.375rem;
+        height: 3.375rem;
+        border-radius: 1.6875rem;
     }
+
+    .user-txt {
+        margin-left: 18px;
+        margin-right: 14px;
+    }
+`;
+
+export const UpdateBtn = styled.button`
+    width: 2.625rem;
+    height: 1.25rem;
+    border-radius: 4px;
+    border: solid 1px ${Palette.Grey[300]};
+    color: ${Palette.Grey[500]};
+    font-size: 0.75rem;
+    font-weight: 500;
+    background-color: ${Palette.White};
+    letter-spacing: -0.17px;
 `;
 
 export const LogoutBtn = styled.button`
-    width: 7rem;
-    padding: 1rem 2rem;
     border: none;
-    background-color: #fee500;
     cursor: pointer;
+    color: ${Palette.Grey[500]};
+    font-size: 0.75rem;
+    background-color: ${Palette.White};
 `;
 
-export const UserImg = styled(HiUser)`
-    width: 5rem;
-    height: 5rem;
-`;
-
-export const UserTab = styled.div`
+export const Locations = styled.div`
     display: flex;
-    height: 40%;
+    height: 100%;
     flex-direction: column;
-    padding: 0 1.5rem;
-
-    .tabs {
-        display: flex;
-        width: 200px;
-        height: 200px;
-
-        h2 {
-            margin: 1rem;
-            pointer: cursor;
-        }
-    }
+    padding: 1rem 1.25rem;
+    background-color: ${Palette.Grey[100]};
+}
 `;
 
-export const Tab = styled.div<{ active: boolean }>`
-    height: 500px;
-    background-color: #e4e9e8;
-    opacity: ${({ active }) => (active ? 1 : 0)};
+export const LocationCard = styled.div`
+    display: flex;
+    background-color: ${Palette.White};
+    width: 23.4rem;
+    padding: 1.25rem 1.125rem;
+    border-radius: 0.375rem;
+    margin-bottom: 0.75rem;
+    cursor: pointer;
+    justify-content: space-between;
 
-    .boxed {
-        display: inline-block;
-        width: 100%;
-        height: 100%;
+    .location-address {
+        font-size: 0.75rem;
+        letter-spacing: -0.010625rem;
+        color: #7f7f7f;
+        margin-bottom: 0.1875rem;
+    }
+
+    .location-title {
         font-size: 1rem;
-        font-weight: bold;
+        letter-spacing: -0.014375rem;
+        color: #333333;
+        margin-bottom: 0.25rem;
+    }
+
+    .address {
+        width: 1.6875rem;
+        height: 0.9375rem;
+        border-radius: 0.1875rem;
+        border: solid 1px #e8e8e8;
+        font-size: 0.625rem;
+        letter-spacing: -0.00875rem;
+        color: #bebebe;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 0.25rem;
     }
 `;
