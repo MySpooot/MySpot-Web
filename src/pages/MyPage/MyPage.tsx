de0736bde689f1b3f196cce845c04a71da4fa643
haeunbae <baehaeun96@gmail.com>
2022-03-07 11:18:42 +0900

    delete myLocation

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import { useMutation, useQuery } from 'react-query';
 
