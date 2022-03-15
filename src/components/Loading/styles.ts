939c6c70ae2bb3929178c6cdd375feca3d65be78
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-03-15 22:19:56 +0900

    stylelint

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -4,24 +4,25 @@ import { Color } from 'src/Constants';
 
 export const Container = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
     height: 100%;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Spinner = styled.div`
+    position: relative;
     width: 50px;
     height: 50px;
-    border-radius: 50%;
     border: 5px solid ${Color.blue};
     border-bottom: 5px solid ${Color.grey[300]};
     animation: spin 1s linear infinite;
-    position: relative;
+    border-radius: 50%;
 
     @keyframes spin {
         from {
             transform: rotate(0deg);
         }
+
         to {
             transform: rotate(360deg);
         }
