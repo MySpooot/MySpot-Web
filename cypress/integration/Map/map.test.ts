08d24d8ec3ba25b1842b9f0d55f74fbc8204c621
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-03-20 01:01:41 +0900

    Private code modal style fix (#36)

--- cypress/integration/Map/map.test.ts
+++ cypress/integration/Map/map.test.ts
@@ -16,3 +16,17 @@ describe('Map', () => {
         cy.getByTestId('roadAddress').should('have.text', '서울 강동구 천호대로 지하 1097');
     });
 });
+
+describe('Private Map', () => {
+    before(() => {
+        Setup.privateMap();
+    });
+
+    it('should show code input', () => {
+        cy.get('h3').should('have.text', '접근이 제한되어 있는 지도');
+
+        const input = cy.findByPlaceholderText('보안코드를 입력해주세요.');
+        input.type('12345');
+        input.invoke('val').should('eq', '1234');
+    });
+});
--- src/components/PrivateCodeModal/styles.ts
+++ src/components/PrivateCodeModal/styles.ts
@@ -6,16 +6,39 @@ export const Container = styled.div`
     display: flex;
     width: 25rem;
     flex-direction: column;
+    align-items: center;
     padding: 1.25rem;
     background-color: ${Color.white};
 `;
 
-export const Input = styled.input``;
+export const Title = styled.h3`
+    font-size: 1.125rem;
+    font-weight: 700;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    padding: 0.75rem;
+    border: 1px solid ${Color.grey[300]};
+    margin: 1.25rem;
+    border-radius: 0.25rem;
+    outline: none;
+
+    &::placeholder {
+        color: ${Color.grey[400]};
+    }
+`;
 
 export const Button = styled.button`
-    cursor: pointer;
+    width: 100%;
+    padding: 0.75rem 0;
+    background-color: ${Color.blue};
+    border-radius: 0.25rem;
+    color: ${Color.white};
+    line-height: 1.25rem;
 
     :disabled {
-        cursor: auto;
+        background-color: ${Color.grey[100]};
+        color: ${Color.grey[600]};
     }
 `;
