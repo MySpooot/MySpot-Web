42596339f0719d9122249f86a84b03d1fa4d55f2
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-03-24 00:10:03 +0900

    Apply like_on icon & Fix like button bug (#44)

--- src/components/MapDetailFooter/MapDetailFooter.tsx
+++ src/components/MapDetailFooter/MapDetailFooter.tsx
@@ -11,6 +11,7 @@ import Button from 'src/components/Button';
 import icArrowLeft from 'src/assets/mymap/ic_arrow_left.svg';
 import icBookmark from 'src/assets/mymap/ic_bookmark.svg';
 import icMarkedBookmark from 'src/assets/mymap/ic_marked_bookmark.svg';
+import icLikeOn from 'src/assets/mymap/ic_like_on.svg';
 import icLikeOff from 'src/assets/mymap/ic_like_off.svg';
 
 type MapDetailFooterProps = {
@@ -44,8 +45,7 @@ const MapDetailFooter: FC<MapDetailFooterProps> = ({ marker, viewButton }) => {
         <Container>
             <Top>
                 <LikeArea onClick={onLikeClick}>
-                    {/* @TODO: 아이콘 추가 */}
-                    <LikeIcon src={marker.isLike ? '' : icLikeOff} />
+                    <LikeIcon src={marker.isLike ? icLikeOn : icLikeOff} />
                     <div className='count'>{marker.likeCount}</div>
                 </LikeArea>
                 <BookmarkIcon src={marker.isMyLocation ? icMarkedBookmark : icBookmark} onClick={onBookmarkClick} />
