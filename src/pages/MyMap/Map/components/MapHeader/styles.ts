efa51240b209a6849949605ca82b79ac801bf0a2
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-03-23 23:32:05 +0900

    FIx font & Create Tooltip button

--- src/pages/MyMap/Map/components/MapHeader/styles.ts
+++ src/pages/MyMap/Map/components/MapHeader/styles.ts
@@ -22,7 +22,9 @@ export const Tooltip = styled.div`
     position: absolute;
     z-index: 11;
     top: 0;
+    display: flex;
     width: 10rem;
+    flex-direction: column;
     padding: 1rem 0.75rem;
     background-color: ${Color.blue};
     border-radius: 0.25rem;
@@ -42,11 +44,24 @@ export const Tooltip = styled.div`
 
     h3 {
         font-size: 0.75rem;
+        font-weight: 500;
         line-height: 1.5;
     }
+`;
 
-    div {
-        font-size: 0.625rem;
-        line-height: 1.3;
-    }
+export const TooltipDescription = styled.div`
+    font-size: 0.625rem;
+    line-height: 1.3;
+`;
+
+export const TooltipButton = styled.button`
+    width: fit-content;
+    align-self: flex-end;
+    padding: 0.125rem 0.625rem;
+    margin-top: 0.5rem;
+    background-color: ${Color.white};
+    border-radius: 0.125rem;
+    color: ${Color.blue};
+    font-size: 0.625rem;
+    line-height: 1.3;
 `;
