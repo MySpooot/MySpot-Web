b504539641279ff270acfdf16392d087b013759b
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-04-23 21:25:26 +0900

    refactor: add vite checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import htmlEnvPlugin from 'vite-plugin-html-env';
+import checker from 'vite-plugin-checker';
 import path from 'path';
 
 export default defineConfig({
     build: { outDir: 'build' },
-    plugins: [reactRefresh(), htmlEnvPlugin()],
+    plugins: [reactRefresh(), htmlEnvPlugin(), checker({ typescript: true, eslint: { lintCommand: 'eslint --ext ts,tsx ./' }, overlay: false })],
     resolve: {
         alias: {
             src: path.resolve(__dirname, './src')
