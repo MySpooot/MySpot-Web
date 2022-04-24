dce3486bb5a8bd3231cbe56de444d393e2aa12b6
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-04-25 01:27:04 +0900

    fix: condition of access permission to search page

--- src/pages/MyMap/Map/components/MapHeader/MapHeader.tsx
+++ src/pages/MyMap/Map/components/MapHeader/MapHeader.tsx
@@ -3,41 +3,49 @@ import { useNavigate, useParams, Link } from 'react-router-dom';
 
 import { Title, RightArea, HeaderIcon, Tooltip, TooltipDescription, TooltipButton } from './styles';
 import { Path } from 'src/Constants';
+import { getMapDetailHelper, getMarkersHelper } from 'src/query';
 import HeaderWithLeftArrow from 'src/components/HeaderWithLeftArrow';
 
 import icSearch from 'src/assets/mymap/ic_search.svg';
 
-type MapHeaderProps = {
-    mapName: string;
-    showTooltip: boolean;
-};
-
-const MapHeader: FC<MapHeaderProps> = ({ mapName, showTooltip }) => {
+const MapHeader: FC = () => {
     const navigate = useNavigate();
     const { mapId } = useParams<{ mapId: string }>();
+
+    const { data: mapDetail } = getMapDetailHelper.useQuery(Number(mapId));
+    const { data: markers } = getMarkersHelper.useQuery(Number(mapId));
     const [openToolTip, setOpenToolTip] = useState(true);
+
+    if (!mapDetail || !markers) {
+        return <></>;
+    }
 
     return (
         <HeaderWithLeftArrow style={{ justifyContent: 'space-between' }} onLeftArrowClick={() => navigate(Path.home)}>
-            <Title>{mapName}</Title>
-            <RightArea>
-                <Link to={`${Path.myMap}/${mapId}${Path.search}`}>
-                    <HeaderIcon alt='search' src={icSearch} />
-                </Link>
+            <Title>{mapDetail.name}</Title>
 
-                {showTooltip && openToolTip && (
-                    <Tooltip>
-                        <h3>이용 Tip</h3>
-                        <TooltipDescription>
-                            저장되어 있는 장소가 없어요!
-                            <br />
-                            검색으로 나만의 장소를 추가해 보세요.
-                        </TooltipDescription>
+            {mapDetail.isOwner ? (
+                <RightArea>
+                    <Link to={`${Path.myMap}/${mapId}${Path.search}`}>
+                        <HeaderIcon alt='search' src={icSearch} />
+                    </Link>
 
-                        <TooltipButton onClick={() => setOpenToolTip(false)}>확인</TooltipButton>
-                    </Tooltip>
-                )}
-            </RightArea>
+                    {markers.length === 0 && openToolTip && (
+                        <Tooltip>
+                            <h3>이용 Tip</h3>
+                            <TooltipDescription>
+                                저장되어 있는 장소가 없어요!
+                                <br />
+                                검색으로 나만의 장소를 추가해 보세요.
+                            </TooltipDescription>
+
+                            <TooltipButton onClick={() => setOpenToolTip(false)}>확인</TooltipButton>
+                        </Tooltip>
+                    )}
+                </RightArea>
+            ) : (
+                <div />
+            )}
         </HeaderWithLeftArrow>
     );
 };
