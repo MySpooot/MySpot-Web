812d60fe05ac609525587767f9f5e2550a7f1b7b
haeunbae <baehaeun96@gmail.com>
2022-05-02 07:03:26 +0900

    new map. home style update

--- src/pages/NewMap/styles.ts
+++ src/pages/NewMap/styles.ts
@@ -11,38 +11,64 @@ export const Container = styled.div`
     max-width: ${Dimension.MaxWidth};
     height: 100vh;
     flex-direction: column;
-    padding: 1.0625rem;
     background-color: #ffffff;
     overflow-y: hidden;
     transition: height 0.5s;
 `;
 
+export const Main = styled.div`
+    padding: 1.25rem;
+    height: calc(100vh - 9.5rem);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
 export const Title = styled.div`
-    margin-top: 7.625rem;
-    color: #333;
-    font-size: 1.625rem;
-    font-weight: bold;
-    letter-spacing: 0.01125rem;
-    line-height: 1;
+    margin-top: 2rem;
+    margin-bottom: 2.5rem;
+    color: #008fff;
+    font-weight: 500;
+    line-height: 1.375rem;
 `;
 
 export const Desc = styled.div`
-    margin-top: 0.3125rem;
-    color: ${Color.grey[500]};
-    font-size: 1rem;
-    font-weight: 300;
-    letter-spacing: 0.006875rem;
-    line-height: 2;
+    margin-bottom: 2.75rem;
+    color: #000000;
+    font-size: 1.625rem;
+    line-height: 2.25rem;
+    font-weight: 500;
 `;
 
 export const InputArea = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 1.875rem;
+
+    input {
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 1.25rem;
+    }
+
+    img {
+        width: 1.375rem;
+        height: 1.375rem;
+    }
+`;
+
+export const ClickPrivate = styled.div`
+    cursor: pointer;
+    display: flex;
+    alignitems: center;
+    width: fit-content;
 `;
 
 export const ButtonArea = styled.div`
     display: flex;
     justify-content: space-between;
     margin-top: auto;
+    Button {
+        height: 4.125rem;
+        color: #7f7f7f;
+    }
 `;
