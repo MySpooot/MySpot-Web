2a9f5550071b4033431689ce16966a100eb73dac
InJaEE <45154110+InJaEE@users.noreply.github.com>
2022-05-12 01:05:13 +0900

    fix: background image

--- src/App.tsx
+++ src/App.tsx
@@ -120,9 +120,6 @@ const AppContainer = styled(Div100vh)`
 const Background = styled.img`
     position: absolute;
     z-index: -1;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    bottom: 0;
+    left: 15%;
 `;
