e1bb540f5cfab107b537259f9c7f320d69675d05
haeunbae <baehaeun96@gmail.com>
2022-05-12 23:11:24 +0900

    mypage header fix

--- src/components/NicknameModal/NicknameModal.tsx
+++ src/components/NicknameModal/NicknameModal.tsx
@@ -1,11 +1,13 @@
 import React, { FC, useState, useCallback } from 'react';
 
-import { Container, Title, BtnArea } from './styles';
+import { Container, CloseHeader, Title, BtnArea } from './styles';
 import { updateUserNicknameMypage } from 'src/api/auth';
 import { useMeState } from 'src/atoms';
 import Modal from 'src/components/Modal';
 import Button from 'src/components/Button';
 import Input from 'src/components/Input';
+
+import close from 'src/assets/mymap/ic_close.svg';
 
 interface NicknameProps {
     setClose: () => void;
@@ -27,19 +29,21 @@ const NicknameModal: FC<NicknameProps> = ({ setClose }) => {
     return (
         <Modal>
             <Container>
-                <Title>닉네임</Title>
+                <CloseHeader>
+                    <img src={close} style={{ cursor: 'pointer' }} onClick={setClose}></img>
+                </CloseHeader>
+                <Title>닉네임을 변경하시겠어요?</Title>
+                {/* <div>현재 사용하고 있는 닉네임 : {me?.nickname}</div> */}
                 <Input
                     maxLength={30}
+                    placeholder='변경할 닉네임 입력'
                     style={{ margin: '1.25rem' }}
                     value={inputValue}
                     autoFocus
                     onChange={event => setInputValue(event.target.value)}
                 />
                 <BtnArea>
-                    <Button className='btn-half' popup onClick={setClose}>
-                        닫기
-                    </Button>
-                    <Button className='btn-half' type='primary' popup onClick={() => saveNickname(inputValue)}>
+                    <Button className='full-half' type='primary' popup onClick={() => saveNickname(inputValue)}>
                         저장하기
                     </Button>
                 </BtnArea>
